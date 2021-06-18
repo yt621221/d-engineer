589db2d1288c77c4b20de7d4c9b9f86f4f029951
yt621221 <yt621221@gmail.com>
2021-06-19 05:48:33 +0900

    不要テーマの削除

--- d-engineer-child/function.php
+++ d-engineer-child/function.php
@@ -3,3 +3,5 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// 親テーマのcssに子テーマのcssを上書きする
--- d-engineer/functions.php
+++ d-engineer/functions.php
@@ -178,3 +178,4 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+
